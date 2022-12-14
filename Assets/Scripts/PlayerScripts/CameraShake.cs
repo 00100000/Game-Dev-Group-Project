eea6a31c824ba5dfa16886e17c4bbcab310f1136
Spetch1111 <samuellau0629@gmail.com>
2022-12-14 14:31:15 -0500

    Sam- I flipped the prefabs and made a level one scene

---
+++ Assets/Scripts/PlayerScripts/CameraShake.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour {
+/*
+	IEnumerator Shake(float duration, float magnitude) {
+		Vector3 originalPos = transform.localPosition;
+		float elapsed = 0.0f;
+		while (elapsed < duration) {
+			elapsed = time.deltaTime;
+		}
+	}
+	*/
+}
---
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	public CharacterController controller;
+
+	public float speed = 8f;
+	public float gravity = -20f;
+	public float jumpHeight = 2f;
+
+	public float groundDistance = 1;
+	public Transform groundCheck;
+	public LayerMask groundMask;
+
+	Vector3 velocity;
+	//bool isGrounded;
+
+	void start()
+    {
+
+    }
+	void Update() {
+		if (isGrounded() && velocity.y < 0) velocity.y = -2f;
+
+		float x = Input.GetAxis("Horizontal");
+		float z = Input.GetAxis("Vertical");
+		// forward
+		Vector3 vec = transform.right * x + transform.forward * z;
+		controller.Move(vec * speed * Time.deltaTime);
+		// jump
+		if (Input.GetButtonDown("Jump") && isGrounded()) {
+			velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+		}
+		// falling
+		velocity.y += gravity * Time.deltaTime;
+		controller.Move(velocity * Time.deltaTime);
+	}
+	public bool isGrounded()
+    {
+		//change this
+		return Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+	}
+}
