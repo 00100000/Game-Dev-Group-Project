c236d0b3d071671e965bdceb7538da1f45d8e394
Spetch1111 <samuellau0629@gmail.com>
2022-12-23 14:34:39 -0500

    a

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerMovement : MonoBehaviour {
 	public CharacterController controller;
@@ -12,6 +13,10 @@ public class PlayerMovement : MonoBehaviour {
 	public float groundDistance = 1;
 	public Transform groundCheck;
 	public LayerMask groundMask;
+
+	private int points = 0;
+
+	public TextMeshProUGUI text;
 
 	Vector3 velocity;
 	//bool isGrounded;
@@ -39,17 +44,34 @@ public class PlayerMovement : MonoBehaviour {
 		Vector3 vec = transform.right * x + transform.forward * z;
 		controller.Move(vec * speed * Time.deltaTime);
 		// jump
+		/*
 		if (Input.GetButtonDown("Jump") && isGrounded()) {
 			velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
 		}
 		// falling
 		
-		controller.Move(velocity * Time.deltaTime);
+		controller.Move(velocity * Time.deltaTime); */
 	}
 	public bool isGrounded()
     {
 		//change this
 		//return Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-		return false;
+		bool rc = Physics.Raycast(groundCheck.position, new Vector3(0, -groundDistance, 0), groundMask);
+		Debug.Log(rc);
+
+		return rc;
 	}
+    private void OnCollisionEnter(Collision collision)
+    {
+		points++;
+		text.SetText(points + " points");
+	}
+
+	/*
+    private void OnCollisionExit(Collision collision)
+    {
+		if (collision.gameObject.CompareTag("Ground"))
+			isG = false;
+    }
+	*/
 }
